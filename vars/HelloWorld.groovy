1c6a436746a0297f3a6de0d39a632421a068b77c
shr2911 <68686711+shr2911@users.noreply.github.com>
2020-07-30 15:09:46 +0530

    Create HelloWorld.groovy

---
+++ vars/HelloWorld.groovy
@@ -0,0 +1,17 @@
+import org.dcube.build.HelloWorld
+import org.dcube.ioc.ContextRegistry
+
+def call(String solutionPath) {
+    ContextRegistry.registerDefaultContext(this)
+
+    def helloworld = new HelloWorld(solutionPath)
+    helloworld.build()
+}import org.dcube.build.HelloWorld
+import org.dcube.ioc.ContextRegistry
+
+def call(String solutionPath) {
+    ContextRegistry.registerDefaultContext(this)
+
+    def helloworld = new HelloWorld(solutionPath)
+    helloworld.build()
+}
